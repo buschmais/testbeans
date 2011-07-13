d26fb1481f53f955a5ddfb0322468833539172b8
Dirk Mahler <dirk.mahler@buschmais.com>
2011-07-13 13:11:00 +0200

    gh-2: added a test suite.

---
+++ junit/test/src/test/java/com/buschmais/testbeans/junit/test/context/ContextTest.java
@@ -0,0 +1,12 @@
+package com.buschmais.testbeans.junit.test.context;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite.SuiteClasses;
+
+import com.buschmais.testbeans.junit.extension.WeldSuite;
+
+@SuiteClasses({ ContextTest1.class, ContextTest2.class })
+@RunWith(WeldSuite.class)
+public class ContextTest {
+
+}
