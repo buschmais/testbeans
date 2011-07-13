7e774f3f7af9b31f967c84ae7af04ca9e7fb5be3
Dirk Mahler <dirk.mahler@buschmais.com>
2011-07-13 13:13:13 +0200

    gh-2: added javadoc for test suite.

--- junit/test/src/test/java/com/buschmais/testbeans/junit/test/context/ContextTest.java
+++ junit/test/src/test/java/com/buschmais/testbeans/junit/test/context/ContextTest.java
@@ -5,6 +5,11 @@ import org.junit.runners.Suite.SuiteClasses;
 
 import com.buschmais.testbeans.junit.extension.WeldSuite;
 
+/**
+ * Defines a suite using {@link WeldSuite}.
+ * 
+ * @author dirk.mahler
+ */
 @SuiteClasses({ ContextTest1.class, ContextTest2.class })
 @RunWith(WeldSuite.class)
 public class ContextTest {
